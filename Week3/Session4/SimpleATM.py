fb15b5ecb9f13eb85a42c1594080a90fee666959
Laiye <laiyen165@gmail.com>
2026-01-08 13:42:52 +0000

    Loop applications

---
+++ Week3/Session4/SimpleATM.py
@@ -0,0 +1,38 @@
+def Deposit(amount):
+    balance += amount
+    return balance
+
+def Withdraw(amount):
+    balance -= amount
+    return balance
+
+
+balance = 0
+while True:
+    print(f"\n1. Deposit\n"
+          f"2. Withdraw\n"
+          f"3. Show balance\n"
+          f"0. Exit")
+    choice = int(input("Enter your choice: \n"))
+    match choice:
+        case 1:
+            amount = int(input("Enter the amount of money you want to deposit\n"))
+            Deposit(amount)
+            print(f"You deposited {amount}\n"
+                  f"Your new balance is {balance}")
+            input()
+        case 2:
+            amount = int(input("Enter the amount of money you want to withdraw\n"))
+            Withdraw(amount)
+            print(f"You withdrew {amount}\n"
+                  f"Your new balance is {balance}")
+            input()
+        case 3:
+            print(f"Your current balance is {balance}")
+            input()
+        case 0:
+            print("Existing")
+            break
+        case _:
+            print("Invalid choice")
+            input()
